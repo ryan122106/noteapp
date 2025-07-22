fc0f396a7a0531f72c0e8e9ca2bd8b190fab8a52
Ryan <ryan.sensei21@gmail.com>
2025-07-22 13:30:17 +0800

    c

--- src/pages/CategoriesPage.jsx
+++ src/pages/CategoriesPage.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { Link as RouterLink } from "react-router";
 import { Box, Button, Paper, TextField, Typography } from "@mui/material";
 import Container from "@mui/material/Container";
-import DeleteIcon from '@mui/icons-material/Delete';
+import DeleteIcon from "@mui/icons-material/Delete";
 import DriveFileRenameOutlineIcon from "@mui/icons-material/DriveFileRenameOutline";
 
 function CategoriesPage() {
@@ -27,56 +27,56 @@ function CategoriesPage() {
           mt: "20px",
         }}
       >
-        <Box sx={{display: "flex"}}>
-
-        <TextField
-          fullWidth
-          id="note_title"
-          label="Title"
-          variant="outlined"
-          value={title}
-          onChange={(event) => setTitle(event.target.value)}
-        />
-        <Button sx={{bgcolor: "lightblue" , color: "white" , ml: "10px"}}>Add</Button>
+        <Box sx={{ display: "flex" }}>
+          <TextField
+            fullWidth
+            id="note_title"
+            label="Title"
+            variant="outlined"
+            value={title}
+            onChange={(event) => setTitle(event.target.value)}
+          />
+          <Button sx={{ bgcolor: "lightblue", color: "white", ml: "10px" }}>
+            Add
+          </Button>
         </Box>
         <Box sx={{ mt: "20px" }}>
           <Typography variant="h4" sx={{ mb: "10px" }}>
             Existing Content (3)
           </Typography>
-          <Box sx={{ml: "20px"}}>
-            <Box sx={{display: "flex"}}>
-          <Typography variant="h6" sx={{ mb: "10px",}}>
-            Personal
-          </Typography>
-          <Box sx={{ml: "660px" , display: "flex"}}>
-            <DriveFileRenameOutlineIcon />
-            <DeleteIcon />
-          </Box>
+          <Box sx={{ ml: "20px" }}>
+            <Box sx={{ display: "flex" }}>
+              <Typography variant="h6" sx={{ mb: "10px" }}>
+                Personal
+              </Typography>
+              <Box sx={{ ml: "660px", display: "flex" }}>
+                <DriveFileRenameOutlineIcon />
+                <DeleteIcon />
+              </Box>
             </Box>
           </Box>
-          <Box sx={{ml: "20px"}}>
-            <Box sx={{display: "flex"}}>
-          <Typography variant="h6" sx={{ mb: "10px",}}>
-            Work
-          </Typography>
-          <Box sx={{ml: "693px" , display: "flex"}}>
-            <DriveFileRenameOutlineIcon />
-            <DeleteIcon />
-          </Box>
+          <Box sx={{ ml: "20px" }}>
+            <Box sx={{ display: "flex" }}>
+              <Typography variant="h6" sx={{ mb: "10px" }}>
+                Work
+              </Typography>
+              <Box sx={{ ml: "693px", display: "flex" }}>
+                <DriveFileRenameOutlineIcon />
+                <DeleteIcon />
+              </Box>
             </Box>
           </Box>
-          <Box sx={{ml: "20px"}}>
-            <Box sx={{display: "flex"}}>
-          <Typography variant="h6" sx={{ mb: "10px",}}>
-            Ideas
-          </Typography>
-          <Box sx={{ml: "690px" , display: "flex"}}>
-            <DriveFileRenameOutlineIcon />
-            <DeleteIcon />
-          </Box>
+          <Box sx={{ ml: "20px" }}>
+            <Box sx={{ display: "flex" }}>
+              <Typography variant="h6" sx={{ mb: "10px" }}>
+                Ideas
+              </Typography>
+              <Box sx={{ ml: "690px", display: "flex" }}>
+                <DriveFileRenameOutlineIcon />
+                <DeleteIcon />
+              </Box>
             </Box>
           </Box>
-
         </Box>
       </Paper>
     </Container>
